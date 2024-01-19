43a24398b8956b05f18fdbb876851e9d699d1f14
coonjaime <48185068+coonjaime@users.noreply.github.com>
2024-01-19 17:07:25 -0500

    Initial commit
    Cleaning and Graphing of Daub Data

---
+++ Daub_Cleaning_Code.R
@@ -0,0 +1,143 @@
+#DAUB ANALYSIS - ALL YEARS OMG #
+#.....................................................................----
+#1. METADATA                                                          ####
+#.....................................................................----
+
+#Authors of this code: Jaime Coon.
+
+#Abbreviations defined:
+##235 - Kellerton 235
+##BSH - Besh Offset
+##DUN - Dunn Ranch
+##GIL - Gilleland
+##KLN - Kellerton North
+##KLL - Kell (Prairie Chicken Area)
+##KLT - Kellerton Tauke
+##LTR - Lee Trail Road
+##RC2 - Richardson 2 (Richardson Hay)
+##RCH2007 - Richardson from 2007-2013
+##RCH2014 - Richardson from 2014-Present
+##RNR - Ringgold North Richardson
+##RIS - Ringgold South
+##RIN - Ringgold North
+##RIE - Ringgold East
+##PAW - Pawnee Prairie
+##PYN - Pyland North
+##PYS - Pyland South
+##PYW - Pyland West
+
+#.....................................................................----
+#2. SETUP                                                          ####
+#.....................................................................----
+
+#ggplot themes
+theme_bar_noleg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        # axis.title.y=element_blank(),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("white"),
+        panel.grid=element_blank(),
+        legend.position="none",
+        legend.text=element_text(size=12, color="black"))}
+
+theme_bar_yaxisnoleg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("snow2"),
+        panel.grid=element_blank(),
+        legend.position="none",
+        legend.text=element_text(size=12, color="black"))}
+theme_bar_leg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("snow2"),
+        panel.grid=element_blank(),
+        legend.position="right",
+        legend.title=element_text(face="bold", size=12),
+        legend.text=element_text(size=12, color="black"))}
+
+
+#.....................................................................----
+#3. IMPORTING AND CLEANING DATA                                       ####
+#.....................................................................----
+setwd("~/Dropbox/_Manuscripts/DaubCode/Daub-Analysis-All")
+
+#install.packages('easypackages')
+library('easypackages')
+packages('tidyverse','janitor','TMB','glmmTMB','TMB','ggeffects','AICcmodavg','readxl','writexl','patchwork')
+
+
+VegData<-read_csv("DaubData_WithMetadata_2007-2018,2021-2023_JJC_9.22.23.csv")%>%
+  mutate(avg_robel=(Robel.N+Robel.E+Robel.S+Robel.W)/4)%>%
+  rowwise() %>% 
+  mutate(sd_robel     = sd(c_across(Robel.N:Robel.E)))%>%
+  mutate(CSG_FEAR     = if_else(Year < 2022, NA_character_, CSG_FEAR))%>%
+  mutate(CSG_FEAR     = if_else(Year < 2022, NA_character_, CSG_FEAR))%>%
+  mutate(Litter_depth = if_else(Litter_depth > 80, NA_character_, Litter_depth))%>%
+  
+  mutate(Sedges       = fct_recode(Sedges, NULL = "."),
+         Sedges       = as.numeric(as.character(Sedges)),
+         
+         CSG_FEAR     = fct_recode(CSG_FEAR, NULL = "."),
+         CSG_FEAR     = as.numeric(as.character(CSG_FEAR)),
+         
+         Violets      = fct_recode(Violets, NULL = "."),
+         Violets      = as.numeric(as.character(Violets)),
+         
+         Panic        = fct_recode(Panic, NULL = "."),
+         Panic        = as.numeric(as.character(Panic)),
+         
+         Seed_mix     = fct_recode(Seed_mix, NULL = "."),
+         Seed_mix     = as.numeric(as.character(Seed_mix)),
+         
+         Litter_depth = fct_recode(Litter_depth, NULL = "."),
+         Litter_depth = as.numeric(as.character(Litter_depth)))%>%
+  #filter(Year>2007)%>%
+  clean_names()%>%
+  mutate(litter       = ifelse(year <  2012,NA_real_,litter))%>% #this replaces zeros with NAs for years we did not collect data
+  mutate(bare         = ifelse(year <  2012,NA_real_,litter))%>% #this replaces zeros with NAs for years we did not collect data
+  mutate(litter_depth = ifelse(year <  2012,NA_real_,litter_depth))%>%
+  mutate(sedges       = ifelse(year >  2016,NA_real_,sedges))%>%
+  mutate(panic        = ifelse(year <  2014,NA_real_,panic))%>%
+  mutate(seed_cover   = ifelse(year <  2017,NA_real_,seed_cover))%>%
+  mutate(seed_mix     = ifelse(year <  2014,NA_real_,seed_mix))%>%
+  mutate(sdv          = ifelse(year <  2014,NA_real_,sdv))%>%
+  mutate(sdv          = ifelse(year == 2017,NA_real_,sdv))%>%
+  mutate(sdv          = ifelse(year == 2017,NA_real_,sdv))%>%
+  mutate(sdv          = ifelse(year == 2018,NA_real_,sdv))%>%
+  mutate(sdv          = ifelse(year == 2021,NA_real_,sdv))
+
+write_csv(VegData,"Veg_Raw_Data.csv")
+
+
+#.....................................................................----
+#3. SAVING  DATA                                       ####
+#.....................................................................----
+
+Veg_Avg_Data_Year = VegData %>%
+  group_by(year)%>%
+  summarise_at(vars(litter,litter_depth:woody,panic:sdv,avg_robel,sd_robel),list(mean="mean",sd="sd"), na.rm=TRUE)
+Veg_Avg_Data_Year
+
+write_csv(Veg_Avg_Data_Year,"Veg_Avg_Data.csv")
+
+Veg_Avg_Data_Pasture = VegData %>%
+  group_by(year,pasture)%>%
+  summarise_at(vars(litter,litter_depth:woody,panic:sdv,avg_robel,sd_robel),list(mean="mean",sd="sd"), na.rm=TRUE)
+Veg_Avg_Data_Pasture
+write_csv(Veg_Avg_Data_Pasture,"Veg_Avg_Data_Pasture.csv")
+
+Veg_Avg_Data_Pasture_Patch = VegData %>%
+  group_by(year,pasture, pasture_patch,pasture_patch_year)%>%
+  summarise_at(vars(litter,litter_depth:woody,panic:sdv,avg_robel,sd_robel),list(mean="mean",sd="sd"), na.rm=TRUE)
+Veg_Avg_Data_Pasture_Patch
+
+write_csv(Veg_Avg_Data_Pasture_Patch,"Veg_Avg_Data_PasturePasture.csv")
+
+
---
+++ Daub_Graphing_Code.R
@@ -0,0 +1,129 @@
+#DAUB ANALYSIS - ALL YEARS OMG #
+#.....................................................................----
+#1. METADATA                                                          ####
+#.....................................................................----
+
+#Authors of this code: Jaime Coon.
+
+#Abbreviations defined:
+##235 - Kellerton 235
+##BSH - Besh Offset
+##DUN - Dunn Ranch
+##GIL - Gilleland
+##KLN - Kellerton North
+##KLL - Kell (Prairie Chicken Area)
+##KLT - Kellerton Tauke
+##LTR - Lee Trail Road
+##RC2 - Richardson 2 (Richardson Hay)
+##RCH2007 - Richardson from 2007-2013
+##RCH2014 - Richardson from 2014-Present
+##RNR - Ringgold North Richardson
+##RIS - Ringgold South
+##RIN - Ringgold North
+##RIE - Ringgold East
+##PAW - Pawnee Prairie
+##PYN - Pyland North
+##PYS - Pyland South
+##PYW - Pyland West
+
+#.....................................................................----
+#2. SETUP                                                          ####
+#.....................................................................----
+
+#ggplot themes
+theme_bar_noleg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        # axis.title.y=element_blank(),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("white"),
+        panel.grid=element_blank(),
+        legend.position="none",
+        legend.text=element_text(size=12, color="black"))}
+
+theme_bar_yaxisnoleg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("snow2"),
+        panel.grid=element_blank(),
+        legend.position="none",
+        legend.text=element_text(size=12, color="black"))}
+theme_bar_leg <- function () { 
+  theme(text=element_text(size=12),
+        axis.title=element_text(face="bold", size=12),
+        axis.text=element_text(size=12,color="black"),
+        axis.line=element_line(color="black",size=1),
+        panel.background=element_rect("snow2"),
+        panel.grid=element_blank(),
+        legend.position="right",
+        legend.title=element_text(face="bold", size=12),
+        legend.text=element_text(size=12, color="black"))}
+
+
+#.....................................................................----
+#3. IMPORTING AND CLEANING DATA                                       ####
+#.....................................................................----
+setwd("~/Dropbox/_Manuscripts/DaubCode/Daub-Analysis-All")
+
+#install.packages('easypackages')
+library('easypackages')
+packages('tidyverse','janitor','TMB','glmmTMB','TMB','ggeffects','AICcmodavg','readxl','writexl','patchwork')
+
+Veg_Pasture       = read_csv("Veg_Avg_Data_Pasture.csv")
+Veg_Pasture_Patch = read_csv("Veg_Avg_Data_PasturePasture.csv")
+Veg_Raw           = read_csv("Veg_Raw_Data.csv")
+
+#.....................................................................----
+#3. GRAPHING  DATA                                       ####
+#.....................................................................----
+#__3a.By Site####
+
+Veg_Pasture_Filtered=Veg_Pasture%>%
+  filter(!pasture=="KELL")%>%
+  filter(!pasture=="NA")%>%
+  filter(!pasture=="RIE")%>%
+  filter(!pasture=="RNR")%>%
+  mutate(pasture=recode(pasture,
+                        "RCH2007"="RCH",
+                        "RCH2014"="RCH"))
+  
+  
+#Bar graph
+FescuePerYear_Plot=ggplot(data=Veg_Pasture_Filtered) +  
+  #geom_line(aes(x=AcademicYear, y=percent_of_total_count_of_id, group=major, color=major),size=1.5)+
+  #geom_point(aes(x=AcademicYear, y=percent_of_total_count_of_id, group=major, color=major),size=3)+
+  #scale_color_manual(values=c("#6fc5b4","#61a382","#446f6b","#6d8aa0","#6acee0","black"))+
+  geom_bar(aes(x=as.factor(year),y=fescue_mean,fill=pasture),stat="identity")+
+  theme_bar_noleg()+ylab("% Cover Tall Fescue")+xlab("Year")+ 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  geom_errorbar(aes(x=as.factor(year),y=fescue_mean,ymin=fescue_mean-fescue_sd,ymax=fescue_mean+fescue_sd))+
+  #scale_x_discrete(expand = c(0.05, 0.05, .25, 0.05)) +
+  #geom_label(aes(x=year,y=fescue,label = fescue), nudge_y=0,nudge_x = .55, size = 3)+
+  facet_wrap(~pasture)
+FescuePerYear_Plot
+#ggsave(BiologyPerYear_Plot,filename="BiologyPerYear_Plot.jpg",dpi=300,units="in",h=5,w=8)
+
+#Boxplot
+#FescuePerYear_Boxplot=ggplot(data=Veg_Raw) +  
+#  geom_boxplot(aes(x=as.factor(year),y=fescue))+
+#  theme_bar_leg()+ylab("% Cover Tall Fescue")+xlab("Year")+ 
+#  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+#  facet_wrap(~pasture)
+#FescuePerYear_Boxplot
+
+
+WSGPerYear_Plot=ggplot(data=Veg_Pasture_Filtered) +  
+  #geom_line(aes(x=AcademicYear, y=percent_of_total_count_of_id, group=major, color=major),size=1.5)+
+  #geom_point(aes(x=AcademicYear, y=percent_of_total_count_of_id, group=major, color=major),size=3)+
+  #scale_color_manual(values=c("#6fc5b4","#61a382","#446f6b","#6d8aa0","#6acee0","black"))+
+  geom_bar(aes(x=as.factor(year),y=wsg_mean,fill=pasture),stat="identity")+
+  theme_bar_noleg()+ylab("% Cover WSG")+xlab("Year")+ 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  geom_errorbar(aes(x=as.factor(year),y=wsg_mean,ymin=wsg_mean-wsg_sd,ymax=wsg_mean+wsg_sd))+
+  #scale_x_discrete(expand = c(0.05, 0.05, .25, 0.05)) +
+  #geom_label(aes(x=year,y=fescue,label = fescue), nudge_y=0,nudge_x = .55, size = 3)+
+  facet_wrap(~pasture)
+WSGPerYear_Plot
